a5fc668ed8dc2cb7dc27ef407e92db59b6633577
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-22 13:08:23 +0000

    Update on 2025-03-22 13:08:23

--- ThePostOffice.c
+++ ThePostOffice.c
@@ -1,40 +1,8 @@
-#include "../TheMaintainer/TheMaintainer.h"
-bool IsNetworkVersion4Used(u8*value);
-bool IsNetworkVersion4Used(u8*value){
-    return GetTheMailConditioner(value,4,false)!=NULL;
+#include "../ThePostOffice/ThePostOffice.h"
+SetupTM(ThePostOffice,0,0,0,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
+static void TMStart(void){
+  
 }
-EXPORT_SYMBOL(IsNetworkVersion4Used);
-struct TheMailConditioner*GetNetworkVersion4(u8*value);
-struct TheMailConditioner*GetNetworkVersion4(u8*value){
-    return GetTheMailConditioner(value,4,true);
+static void TMEnd(void){
+  
 }
-EXPORT_SYMBOL(GetNetworkVersion4);
-bool IsNetworkVersion6Used(u8*value);
-bool IsNetworkVersion6Used(u8*value){
-    return GetTheMailConditioner(value,16,false)!=NULL;
-}
-EXPORT_SYMBOL(IsNetworkVersion6Used);
-struct TheMailConditioner*GetNetworkVersion6(u8*value);
-struct TheMailConditioner*GetNetworkVersion6(u8*value){
-    return GetTheMailConditioner(value,16,true);
-}
-EXPORT_SYMBOL(GetNetworkVersion6);
-bool IsMediaAccessControlUsed(u8*value);
-bool IsMediaAccessControlUsed(u8*value){
-    return GetTheMailConditioner(value,6,false)!=NULL;
-}
-EXPORT_SYMBOL(IsMediaAccessControlUsed);
-struct TheMailConditioner*GetMediaAccessControl(u8*value);
-struct TheMailConditioner*GetMediaAccessControl(u8*value){
-   return GetTheMailConditioner(value,6,true);
-}
-EXPORT_SYMBOL(GetMediaAccessControl);
-static void End(void){
-    //need to be call to the last project
-//    StopExpiryWorkBase();
-
-}
-static void Start(void){
-    
-}
-Setup("ThePostOffice",0,0)
--- ThePostOffice.h
+++ ThePostOffice.h
@@ -1,4 +1,8 @@
 #ifndef ThePostOffice_H
 #define ThePostOffice_H
-
+#include "../TheMaintainer/TheMaintainer.h"
+struct ThePostOffice{
+    u8 test;
+};
+#define GetThePostOfficeTM()TM(ThePostOffice,0,0,0,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
 #endif
