67a3c2a5d314935a6a83b915a5eb434a74acd376
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-24 21:36:25 +0000

    Update on 2025-03-24 21:36:25

--- ThePostOffice.c
+++ ThePostOffice.c
@@ -1,13 +1,16 @@
 #include "ThePostOffice.h"
 #include "../TheNetworkAdapter/TheNetworkAdapter.h"
 SetupTM(ThePostOffice,0,0,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
+
+
+
 static void TMStart(void){
-    GetTheNetworkAdapterTM();
-    TheNetworkAdapterTM->GetAdd();
+    TMKeepAliveStart();
+
     printk(KERN_INFO "ThePostOffice: The ThePostOffice TM has been started.\n");
 }
 static void TMEnd(void*){
-
+    TMKeepAliveEnd();
  
     printk(KERN_INFO "ThePostOffice: The ThePostOffice TM has been ended.\n");
 }
